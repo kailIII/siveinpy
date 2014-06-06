63a1d7ea99875de9a52c306cf87ad1a617f3ecce
Richpolis Systems <richpolis@gmail.com>
2014-06-05 22:39:39 -0500

    Creacion de la vista mostrar listado de autores

--- SiveinPy/SiveinPy/settings.py
+++ SiveinPy/SiveinPy/settings.py
@@ -36,8 +36,9 @@ TEMPLATE_DIRS = (
 MEDIA_ROOT = RUTA_PROYECTO.child('media')
 
 
-# Application definition
+MEDIA_URL = 'http://localhost:8000/media/'
 
+# Application definition
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
--- SiveinPy/SiveinPy/urls.py
+++ SiveinPy/SiveinPy/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.conf import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,10 +10,14 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+	url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT,}), 	
     
     # inicio
     url(r'^',include('apps.inicio.urls')),
 
     # autores
     url(r'^autores/',include('apps.autores.urls')),
+
+
 )
